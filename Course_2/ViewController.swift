450d9a783fb769ff28f5a4e543fe70530ff5d72e
Kirill <kirill.u.stork@gmail.com>
2022-04-28 01:13:12 +0300

    Initial Commit

---
+++ Course_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Course_2
+//
+//  Created by Kirill on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
